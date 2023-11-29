9ee47006ab26be8430bf7da123acdc5dbd55bbca
Mads Mikkel Rasmussen <mara@aspit.dk>
2023-11-29 15:29:22 +0100

    implemented hub send message method

--- SignalrChats/SignalrChats.Server/ChatHub.cs
+++ SignalrChats/SignalrChats.Server/ChatHub.cs
@@ -4,5 +4,7 @@ namespace SignalrChats.Server
 {
     public class ChatHub: Hub
     {
+        public async Task SendMessage(string user, string message)
+            => await Clients.All.SendAsync("HandleNewMessage", user, message);
     }
 }
